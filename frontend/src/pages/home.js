9dd7c98b91d282611c591afde846457d1aa578cb
hh-042 <hannah.hussain044@gmail.com>
2025-10-01 16:15:37 +0100

    Create home.js

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the DNA Uno Homepage!</h1>
+      <p>Select an option from the menu.</p>
+
+        <p>contact us: contact@dnauno.com</p>
+    </div>
+  );
+};
+
+export default Home;
